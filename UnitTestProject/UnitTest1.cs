c5938b1c30ebd6b3ef0ce4c41d75c28c4f02ab01
SAMIRE <infos.adgoup@gmail.com>
2019-03-31 06:35:44 +0000

    Commit n°4 : nunit delete

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,52 +1,52 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IdyalUnitTest;
 
 namespace UnitTestProject
 {
-    [TestFixture]
+    [TestClass]
     public class UnitTest1
     {
     
-        [Test]
+        [TestMethod]
         public void TestMethod1()
         {
             Assert.AreEqual("Hello, we will do some tests", Program.HelloMessage());
         }
 
-        [Test]
+        [TestMethod]
         public void Add()
         {
             Program math = new Program();
             Assert.AreEqual(50, math.Add(20, 30));
         }
-        [Test]
+        [TestMethod]
         public void Sub()
         {
             Program math = new Program();
             Assert.AreEqual(25, math.Sub(40, 15));
         }
-        [Test]
+        [TestMethod]
         public void Prod()
         {
             Program math = new Program();
             Assert.AreEqual(50, math.Prod(5, 10));
         }
-        [Test]
+        [TestMethod]
         public void Diviser()
         {
             Program math = new Program();
             Assert.AreEqual(5, math.Diviser(100, 20));
         }
 
-        [Test]
+        [TestMethod]
         public void ReturnFalseGivenValueOf1()
         {
             Program test = new Program();
             var result = test.IsPrime(1);
             Assert.IsFalse(result, "1 should not be prime");
         }
-        [TestCase(-1)]
+        /*[TestCase(-1)]
         [TestCase(0)]
         [TestCase(1)]
         public void ReturnFalseGivenValuesLessThan2(int value)
@@ -54,8 +54,8 @@ namespace UnitTestProject
             Program test = new Program();
             var result = test.IsPrime(value);
             Assert.IsFalse(result, $"{value} should not be prime");
-        }
-        [Test]
+        }*/
+        [TestMethod]
         public void ReturnTrueGivenValueOf2()
         {
             Program test = new Program();
