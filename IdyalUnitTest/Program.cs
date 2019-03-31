014fda8589856ffd7c0f578ed4d448b12b106978
SAMIRE <infos.adgoup@gmail.com>
2019-03-31 04:51:43 +0000

    First commit

--- IdyalUnitTest/Program.cs
+++ IdyalUnitTest/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace IdyalUnitTest
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(HelloMessage());
+        }
+        public static String HelloMessage()
+        {
+            return "Hello, we will do some tests";
+        }
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 2)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Prod(int a, int b)
+        {
+            return a * b;
+        }
+        public int Diviser(int a, int b)
+        {
+            return a / b;
         }
     }
 }
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IdyalUnitTest;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+    
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello, we will do some tests", Program.HelloMessage());
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            Program math = new Program();
+            Assert.AreEqual(50, math.Add(20, 30));
+        }
+        [TestMethod]
+        public void Sub()
+        {
+            Program math = new Program();
+            Assert.AreEqual(25, math.Sub(40, 15));
+        }
+        [TestMethod]
+        public void Prod()
+        {
+            Program math = new Program();
+            Assert.AreEqual(50, math.Prod(5, 10));
+        }
+        [TestMethod]
+        public void Diviser()
+        {
+            Program math = new Program();
+            Assert.AreEqual(5, math.Diviser(100, 20));
+        }
+
+        [TestMethod]
+        public void ReturnFalseGivenValueOf1()
+        {
+            Program test = new Program();
+            var result = test.IsPrime(1);
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+        [TestMethod]
+        public void ReturnTrueGivenValueOf2()
+        {
+            Program test = new Program();
+            var result = test.IsPrime(2);
+            Assert.IsTrue(result, "2 should be prime");
+        }
+    }
+}
